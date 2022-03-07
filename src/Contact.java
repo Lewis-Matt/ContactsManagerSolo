f716b21cf424ebab8ff0ab7b5cbfa8ee8ec39fbf
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-03-07 10:01:31 -0600

    added structure for contact, constructor, getters, setters

--- src/Contact.java
+++ src/Contact.java
@@ -1,2 +1,40 @@
+
 public class Contact {
+
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+    // CONSTRUCTOR
+    public Contact(String firstName, String lastName, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    // GETTERS
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    // SETTERS
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
