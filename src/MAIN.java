17bbf792301ba4b9821b31311db6e593be32bb9e
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-03-07 10:46:22 -0600

    menu, pseudo code

--- src/MAIN.java
+++ src/MAIN.java
@@ -1,10 +1,13 @@
+import java.io.IOException;
 import java.util.Scanner;
+
+import static util.Input.getInt;
 
 public class MAIN {
 
-    // MENU METHOD
-    // throws exception?
-    public static void menu() {
+    // TODO MENU METHOD
+    // throws IOException?
+    public static void menu() throws IOException {
         System.out.println("1. View contacts.\n" +
                 "2. Add a new contact.\n" +
                 "3. Search a contact by name.\n" +
@@ -12,9 +15,23 @@ public class MAIN {
                 "5. Exit.\n" +
                 "Enter an option [1-5]: ");
 
+        // Store user choice
+        int selectedOption = getInt(1,5);
 
     }
 
 
+    // TODO VIEW CONTACTS
 
+    // TODO ADD CONTACT
+
+    // TODO SEARCH CONTACTS
+
+    // TODO DELETE CONTACT
+
+
+    // ************************* MAIN **************************
+    public static void main(String[] args) throws IOException {
+        menu();
+    }
 }
