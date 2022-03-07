4de47ec79a88bb8cd05879a6bb8a1a0be6a487f0
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-03-07 10:31:45 -0600

    created menu() method

--- src/MAIN.java
+++ src/MAIN.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class MAIN {
+
+    // MENU METHOD
+    // throws exception?
+    public static void menu() {
+        System.out.println("1. View contacts.\n" +
+                "2. Add a new contact.\n" +
+                "3. Search a contact by name.\n" +
+                "4. Delete an existing contact.\n" +
+                "5. Exit.\n" +
+                "Enter an option [1-5]: ");
+
+
+    }
+
+
+
 }
